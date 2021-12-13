f6ea15b71c1ec10857e07c1560af2aeb3589a295
Estevan T. D. Martins <estevandias7@gmail.com>
2021-12-13 20:31:57 -0300

    feat: page for a specific event created;

---
+++ src/pages/user/EventUser/index.tsx
@@ -0,0 +1,113 @@
+import { Back, Container, Button } from "./styles";
+import { BsArrow90DegLeft } from "react-icons/bs";
+import { useNavigate, useParams } from "react-router";
+import api from "../../../services/api";
+import { EventData } from "../../../types/EventContext";
+import { useEffect, useState } from "react";
+import { useAuth } from "../../../contexts/Auth";
+
+export const EventUser = () => {
+  const { id } = useParams();
+  const { token } = useAuth();
+  const [event, setEvent] = useState<EventData>({} as EventData);
+  const [render, setRender] = useState<boolean>(false);
+  const [isSubscribed, setIsSubscribed] = useState<boolean>(false);
+  const user = {
+    id: 1,
+    name: "estevan",
+    email: "estevan@mail.com",
+    points: 0,
+    address: null,
+    event: {
+      id: 1,
+      name: "KenzieHack",
+      date: "Thu, 01 Dec 2022 00:00:00 GMT",
+      duration: "Thu, 01 Dec 2022 00:00:00 GMT",
+      skills: "[testes, teste]",
+      sponsors_id: null,
+      pending: true,
+    },
+  };
+
+  const verifySubscription = () => {
+    if (!!user.event.id) {
+      setIsSubscribed(true);
+    }
+  };
+
+  const getEvent = async () => {
+    const response = await api.get(`/events/${id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const formated = response.data;
+    const date = new Date(formated.date);
+    formated.date = date.toLocaleDateString("pt-BR", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+    });
+    const duration = new Date(formated.duration);
+    formated.duration = duration.toLocaleDateString("pt-BR", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+    });
+    setEvent(formated);
+    setRender(true);
+  };
+
+  useEffect(() => {
+    getEvent();
+    verifySubscription();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const navigate = useNavigate();
+
+  const subscribe = async (id: number) => {
+    const response = await api.patch(`/users/event/signup/${id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    console.log(response.data);
+  };
+
+  console.log(isSubscribed);
+
+  return (
+    <>
+      <Back onClick={() => navigate("/events-user")}>
+        <BsArrow90DegLeft />
+      </Back>
+      {render ? (
+        <Container>
+          <p>
+            Evento: <span>{event.name}</span>
+          </p>
+          <p>
+            Data:{" "}
+            <span>
+              {event.date} até {event.duration}
+            </span>
+          </p>
+          <p>
+            Descrição:
+            <span> {event.description}</span>
+          </p>
+          <p>
+            Habilidades Técnicas:
+            <span> {event.skills.replace(/\]/gm, "").replace(/\[/gm, "")}</span>
+          </p>
+          <Button disabled={isSubscribed} onClick={() => subscribe(user.id)}>
+            Inscreva-se
+          </Button>
+        </Container>
+      ) : (
+        ""
+      )}
+    </>
+  );
+};
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,6 +4,7 @@ import { EventsUser } from "../pages/user/EventsUser/Index";
 import { ConfigUser } from "../pages/user/ConfigUser";
 import { HomeUser } from "../pages/user/HomeUser";
 import { LoginUser } from "../pages/user/LoginUser";
+import { EventUser } from "../pages/user/EventUser";
 
 export const RoutesMain = () => {
   return (
@@ -12,6 +13,7 @@ export const RoutesMain = () => {
       <Route path="/home-user" element={<HomeUser />} />
       <Route path="/login-user" element={<LoginUser />} />
       <Route path="/events-user" element={<EventsUser />} />
+      <Route path="/events-user/:id" element={<EventUser />} />
     </Routes>
   );
 };

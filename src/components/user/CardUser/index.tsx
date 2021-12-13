73d2d5b8e17b4cbefc30cb24a7ed38b42a987abf
Mônica S <monica.s.bergues@gmail.com>
2021-12-13 15:27:59 -0300

    feat: user config page

--- src/components/user/CardUser/index.tsx
+++ src/components/user/CardUser/index.tsx
@@ -1,3 +1,73 @@
+import { useState } from "react";
+import {
+  BsPencilSquare as Pencil,
+  BsPersonCircle as User,
+} from "react-icons/bs";
+import { RiLockPasswordLine as Lock } from "react-icons/ri";
+import { Button } from "../Button";
+import { UpdateData } from "../UpdateData";
+import { UpdatePassword } from "../UpdatePassword";
+import { Container, Content, UpdateBox } from "./style";
+
 export const CardUser = () => {
-  return <div>CardUser</div>;
+  const [updateUser, setUpdateUser] = useState(false);
+  const [updatePassword, setUpdadePassword] = useState(false);
+
+  const handleUser = () => {
+    setUpdateUser(!updateUser);
+  };
+
+  const handlePassword = () => {
+    setUpdadePassword(!updatePassword);
+  };
+
+  const data = [
+    {
+      name: "monica",
+      address: "adress, adress, nº105",
+      CEP: "11423-320",
+      email: "monica@mail",
+    },
+  ];
+  return (
+    <Container>
+      <Content>
+        <div>
+          <User className="icon" />
+        </div>
+        <div>
+          {data.map((elem, index) => {
+            return (
+              <div key={index} className="user-box">
+                <p>Nome: {elem.name}</p>
+                <p>Endereço: {elem.address}</p>
+                <p>Cep: {elem.CEP}</p>
+                <p>Email: {elem.email}</p>
+                <div>
+                  <Button redTheme={false} onClick={handleUser}>
+                    <Pencil />
+                    Alterar dados
+                  </Button>
+                  <Button redTheme={true} onClick={handlePassword}>
+                    <Lock />
+                    Alterar senha
+                  </Button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </Content>
+      {updateUser && (
+        <UpdateBox>
+          <UpdateData handleUser={handleUser} />
+        </UpdateBox>
+      )}
+      {updatePassword && (
+        <UpdateBox>
+          <UpdatePassword handleUser={handleUser} />
+        </UpdateBox>
+      )}
+    </Container>
+  );
 };
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import { LandingPage } from "../pages/landingPage";
+import { ConfigUser } from "../pages/user/ConfigUser";
 import { HomeUser } from "../pages/user/HomeUser";
 import { LoginUser } from "../pages/user/LoginUser";
 

5f8a43d5609a04fa9e120368e070b368e5ec8846
Estevan T. D. Martins <estevandias7@gmail.com>
2021-12-16 23:13:27 -0300

    fix: 'see details' button was redirecting to non-existent page;

--- src/pages/user/HomeUser/index.tsx
+++ src/pages/user/HomeUser/index.tsx
@@ -1,10 +1,10 @@
-import { MenuLateral } from "../../../components/MenuLateral"
+import { MenuLateral } from "../../../components/MenuLateral";
 import { Container, Main } from "./styles";
 import { MenuMobile } from "../../../components/MenuMobile";
 import { CardPoints } from "../../../components/user/CardPoints";
-import { CardSubscription } from '../../../components/user/CardSubscription'
+import { CardSubscription } from "../../../components/user/CardSubscription";
 import { CardEvent } from "../../../components/user/CardEvent";
-import {IoIosArrowDown} from 'react-icons/io'
+import { IoIosArrowDown } from "react-icons/io";
 import api from "../../../services/api";
 import { useEffect, useState } from "react";
 import { useAuth } from "../../../contexts/Auth";
@@ -13,112 +13,103 @@ import { UserData, EventData } from "../../../types/EventContext";
 import PacmanLoader from "react-spinners/PacmanLoader";
 import { useNavigate } from "react-router-dom";
 
-
 export const HomeUser = () => {
-
-  const {token, userId} = useAuth()
-  const [user, setUser] = useState<UserData>({} as UserData)
-  const [message, setMessage] = useState('')
-  const [events, setEvents] = useState<EventData[]>([])
-  const [loading, setLoading] = useState(false)
-  const navigate = useNavigate()
+  const { token, userId } = useAuth();
+  const [user, setUser] = useState<UserData>({} as UserData);
+  const [message, setMessage] = useState("");
+  const [events, setEvents] = useState<EventData[]>([]);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
   const getUser = async () => {
     const response = await api.get<UserData>(`/users/${userId}`, {
       headers: {
         Authorization: `Bearer ${token}`,
-      }
-    })
-    setUser(response.data)
-  }
+      },
+    });
+    setUser(response.data);
+  };
 
   const getAllEvents = async () => {
-    setLoading(true)
+    setLoading(true);
     const response = await api.get(`/events`, {
       headers: {
         Authorization: `Bearer ${token}`,
-      }
-    })
-    setEvents(response.data)
-    setLoading(false)
-  }
+      },
+    });
+    setEvents(response.data);
+    setLoading(false);
+  };
 
   useEffect(() => {
-    getUser()
-    getAllEvents()
-    const hour = new Date().getHours()
-    if (hour >= 12 && hour < 18){
-      setMessage('Boa tarde')
-    }else if(hour >= 6 && hour < 12 ){
-      setMessage('Bom dia')
-    }else{
-      setMessage('Boa noite')
+    getUser();
+    getAllEvents();
+    const hour = new Date().getHours();
+    if (hour >= 12 && hour < 18) {
+      setMessage("Boa tarde");
+    } else if (hour >= 6 && hour < 12) {
+      setMessage("Bom dia");
+    } else {
+      setMessage("Boa noite");
     }
-  }, [])
+  }, []);
 
-    
   const detailsEvent = (params: string) => {
-    navigate('/details-event')
-  } 
-  
-  return(
+    navigate(params);
+  };
+
+  return (
     <Container className="Home">
-      <MenuMobile/>
-      <MenuLateral/> 
+      <MenuMobile />
+      <MenuLateral />
 
       <div className="content">
-      <p className="user"> {message}, <span> {user? user.name: '. . . .'}!</span></p>
-      
-      <Main>
-      <div className="info">
-        <CardPoints
-          title="Pontos"
-          points={user.points}
-          />
+        <p className="user">
+          {" "}
+          {message}, <span> {user ? user.name : ". . . ."}!</span>
+        </p>
 
+        <Main>
+          <div className="info">
+            <CardPoints title="Pontos" points={user.points} />
 
-          <CardSubscription
-            title="Inscrições"
-            event={user.event === null ? 'sem evento': 'com evento'} // arrumar aqui
-            group={user?.group}
+            <CardSubscription
+              title="Inscrições"
+              event={user.event === null ? "sem evento" : "com evento"} // arrumar aqui
+              group={user?.group}
             />
+          </div>
 
-        </div>
-
-        <div className="list-event">
-
+          <div className="list-event">
             <p className="title"> Eventos </p>
 
-             {
-               loading?
-               (
-                 <span className="span-loading">
-                   <PacmanLoader color="#ffff" />
-                 </span>
-               )
-               :
-               (
-                 events.map((event) => {
-                   return <CardEvent
-                      title={event.name}
-                      date={event.date}
-                      key={event.id}
-                      pathFunc="/detials-event"
-                      func={detailsEvent}
-                   />
-                 })
-               )
-             }
+            {loading ? (
+              <span className="span-loading">
+                <PacmanLoader color="#ffff" />
+              </span>
+            ) : (
+              events.map((event) => {
+                return (
+                  <CardEvent
+                    title={event.name}
+                    date={event.date}
+                    key={event.id}
+                    pathFunc={`/events-user/${event.id}`}
+                    func={detailsEvent}
+                  />
+                );
+              })
+            )}
 
-
-            <button className="button-ver-mais"
-                    onClick={() => navigate('/events-user')}
-            >Ver todos <IoIosArrowDown/> </button>
-          </div>  
-                
+            <button
+              className="button-ver-mais"
+              onClick={() => navigate("/events-user")}
+            >
+              Ver todos <IoIosArrowDown />{" "}
+            </button>
+          </div>
         </Main>
       </div>
-
     </Container>
-  )
+  );
 };
